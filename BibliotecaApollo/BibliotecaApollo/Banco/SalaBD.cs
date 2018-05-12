b2353e5204aa705258623b266674b1a984d9cc16
pedronardelli <pedronardelli@PEDRONARDELCFDC>
2018-05-12 13:33:34 -0300

    Criacao pasta BD e DAO. Inclusao SalaBD, SalaDAO.

---
+++ BibliotecaApollo/BibliotecaApollo/Banco/SalaBD.cs
@@ -0,0 +1,46 @@
+﻿using BibliotecaApollo.Basicas;
+using BibliotecaApollo.Conexao;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaApollo.Banco
+{
+    class SalaBD : ConexaoSqlServer
+    {
+        public void InserirSalaNoBanco(SalaBasica sala)
+        {
+            try
+            {
+                //abrir a conexao com o Banco de Dados
+                this.abrirConexao();
+
+                string sql = "insert into sala (sal_valor, sal_nome, sal_descricao) ";
+                sql += "values(@sal_valor, @sal_nome,@sal_descricao)";
+
+                //Instrucao a ser executada no banco de dados.
+                SqlCommand cmd = new SqlCommand(sql, this.sqlConn);
+
+                //Substituindo as strings pelo valor recebido do usuario.
+                cmd.Parameters.Add("@sal_valor", SqlDbType.Decimal);
+                cmd.Parameters["@sal_valor"].Value = sala.Sal_valor;
+
+                cmd.Parameters.Add("@sal_nome", SqlDbType.VarChar);
+                cmd.Parameters["@sal_nome"].Value = sala.Sal_nome;
+
+                cmd.Parameters.Add("@sal_descricao", SqlDbType.Text);
+                cmd.Parameters["@sal_descricao"].Value = sala.Sal_descricao;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex = new Exception("Erro ao Cadastrar una Sala.");
+            }
+        }
+    }
+}
---
+++ BibliotecaApollo/BibliotecaApollo/DAO/SalaDAO.cs
@@ -0,0 +1,45 @@
+﻿using BibliotecaApollo.Banco;
+using BibliotecaApollo.Basicas;
+using BibliotecaApollo.Conexao;
+using BibliotecaApollo.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaApollo.DAO
+{
+    class SalaDAO : ConexaoSqlServer, SalaInterface
+    {
+        public void AlterarSala(SalaBasica sala)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletarSala(SalaBasica sala)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InserirSala(SalaBasica sala)
+        {
+            try
+            {
+                //Criando uma nova sala.
+                SalaBD InserirSala = new SalaBD();
+
+                //Inserindo uma nova sala no Banco de Dados.
+                InserirSala.InserirSalaNoBanco(sala);
+            }
+            catch (Exception ex)
+            {
+
+                throw (ex);
+            }
+            
+        }
+
+        
+    }
+}
