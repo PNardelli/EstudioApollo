fac4058e97693eb1ea7709bf8862167a54688e9b
pedronardelli <pedronardelli@PEDRONARDELCFDC>
2018-05-12 12:06:43 -0300

    Inclusao Interfaces: Artista, Cliente e Instrumento.

---
+++ BibliotecaApollo/BibliotecaApollo/Interface/ArtistaInterface.cs
@@ -0,0 +1,21 @@
+﻿using BibliotecaApollo.Basicas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaApollo.Interface
+{
+    interface ArtistaInterface
+    {
+        //Cadastrar um novo Artista.
+        void InserirArtista(ArtistaBasica artista);
+
+        //Deleta um Artista já cadastrado.
+        void DeletarArtista(ArtistaBasica artista);
+
+        //Altera um Artista já cadastrado.
+        void AlterarArtista(ArtistaBasica artista);
+    }
+}
