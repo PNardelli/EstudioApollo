166f8d17cc43ab563c4a6782d67948478b73e996
pedronardelli <pedronardelli@PEDRONARDELCFDC>
2018-05-12 11:49:55 -0300

    Inclusao das Interface Sala e Horario

---
+++ BibliotecaApollo/BibliotecaApollo/Interface/HorarioInterface.cs
@@ -0,0 +1,24 @@
+﻿using BibliotecaApollo.Basicas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaApollo.Interface
+{
+    /// Implementaçao do Horario.
+    
+    interface HorarioInterface
+    {
+        //Cadastrar um novo horario.
+        void InserirHorario(HorarioBase horario);
+
+        //Deleta um horario já cadastrado.
+        void DeletarHorario(HorarioBase horario);
+
+        //Altera um horario já cadastrado.
+        void AlterarHorario(HorarioBase horario);
+
+    }
+}
